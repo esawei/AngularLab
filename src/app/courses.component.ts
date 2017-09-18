3038162e6be6aa8bdae801f4287922376025d4dd
esawei <esawei5190@gmail.com>
2017-09-19 04:30:06 +0800

    Style Binding

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'courses',
     template: `
-        <button class="bnt btn-primary" [class.active]="isActive">Save</button>
+        <button [style.backgroundColor]="isActive ? 'blue' : 'white'">Save</button>
     `
 })
 export class CoursesComponent {
