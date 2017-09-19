94fbd4dff3f759964e19281aa5bf4c160e56919d
esawei <esawei5190@gmail.com>
2017-09-19 10:22:35 +0800

    45. Event Binding

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -4,9 +4,19 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'courses',
     template: `
-        <button [style.backgroundColor]="isActive ? 'blue' : 'white'">Save</button>
+        <div (click)="onDivClicked()">
+            <button (click)="onSave($event)">Save</button>
+        </div>
     `
 })
 export class CoursesComponent {
-    isActive = true;
+    onDivClicked() {
+        console.log("Div was clicked.");
+    }
+
+    onSave($event) {
+        $event.stopPropagation();
+
+        console.log("Button was clicked.", $event)
+    }
 }
